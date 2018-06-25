4a18f56b8fd8019d5959906954112837e47b9d43
“innocent <rockcoolsaint@gmail.com>
2018-06-25 14:46:00 +0100

    using correct endpoint

--- src/util/ajax.js
+++ src/util/ajax.js
@@ -2,7 +2,7 @@ export default {
     loginendpoint: "https://chef.mybukka.com/api/v1/bukka/auth/custom/login",
     carddtlsendpoint: "https://chef.mybukka.com/api/v1/bukka/customer/card/",
     signupendpoint: "https://chef.mybukka.com/api/v1/bukka/auth/register",
-    chefendpoint: "https://bukka-staging-env.herokuapp.com/api/v1/bukka/chefs/",
+    chefendpoint: "https://salty-escarpment-2400.herokuapp.com/api/v1/bukka/chefs/",
     logo: "https://res.cloudinary.com/www-mybukka-com/image/upload/v1505151382/logo_m8ik1x.png",
     menupageBackground: 'https://www.miami.com/wp-content/uploads/sites/2/2016/12/h-mcdonalds-Double-Quarter-Pounder-with-Cheese-Extra-Value-Meals.png',
     addcard: 'https://chef.mybukka.com/api/v1/bukka/customer/cardDetails/',
